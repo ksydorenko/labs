cae4e1017f1bee527f690f9ad50cfb2d77c51e50
ksydorenko <sydorenko.K.V@nmu.one>
2021-11-01 22:09:18 +0200

    lab3_Sydorenko_124-18-1

---
+++ src/test/java/lab2/controller/FacultyCreatorTest.java
@@ -0,0 +1,33 @@
+package lab2.controller;
+
+import lab2.model.Chair;
+import lab2.model.Faculty;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.List;
+
+class FacultyCreatorTest {
+
+    @Test
+    void createFacultyRandomly() {
+        float result = 0;
+        int count = 30;
+        for (int iter = 0; iter < count; iter++) {
+            FacultyCreator creator = new FacultyCreator();
+            Faculty faculty = creator.createFacultyRandomly();
+            List<Chair> chairs = faculty.getChairs();
+            HashSet<Chair> chairSet = new HashSet<>();
+
+            for (int i = 0; i < chairs.size(); i++) {
+                chairSet.add(chairs.get(i));
+            }
+            if (chairs.size() != chairSet.size()) {
+                result++;
+            }
+        }
+        System.out.println("Percentage of content at the faculty of chairs with the same name: " + result / count * 100 + "%");
+
+   }
+}
---
+++ src/test/java/lab2/controller/HumanCreatorTest.java
@@ -0,0 +1,19 @@
+package lab2.controller;
+
+import lab2.model.Human;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class HumanCreatorTest {
+
+    @Test
+    void createHumanRandomly() {
+        HumanCreator creator = new HumanCreator();
+        Human human = creator.createHumanRandomly();
+        assertTrue(HumanCreator.DICTIONARY_NAMES.contains(human.getName()));
+        assertTrue(HumanCreator.DICTIONARY_SURNAMES.contains(human.getSurname()));
+        assertTrue(HumanCreator.DICTIONARY_DAY_OF_BIRTH.contains(human.getDateOfBirth()));
+
+    }
+}
