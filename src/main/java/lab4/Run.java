d1fbb813075a5fb197ae75a393d2d113d86743e6
ksydorenko <sydorenko.K.V@nmu.one>
2021-11-11 00:29:31 +0200

    lab4_Sydorenko_124-18-1

---
+++ src/main/java/lab4/Run.java
@@ -0,0 +1,14 @@
+package lab4;
+
+import lab2.controller.UniversityCreator;
+import lab2.model.University;
+
+public class Run {
+    public static void main(String[] args) {
+        UniversityJSONReaderWriter universityJSONReaderWriter = new UniversityJSONReaderWriter();
+        UniversityCreator universityCreator = new UniversityCreator();
+        University originalUniversity = universityCreator.createUniversityRandomly();
+        universityJSONReaderWriter.writeToFile(originalUniversity,"./src/main/resources/lab4.txt");
+        University universityFromFile = universityJSONReaderWriter.readFromFile("./src/main/resources/lab4.txt");
+    }
+}
---
+++ src/test/java/lab4/UniversityJSONReaderWriterTest.java
@@ -0,0 +1,25 @@
+package lab4;
+
+import lab2.controller.UniversityCreator;
+import lab2.model.University;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UniversityJSONReaderWriterTest {
+
+    @org.junit.jupiter.api.Test
+    void writeToFile() {
+        UniversityJSONReaderWriter universityJSONReaderWriter = new UniversityJSONReaderWriter();
+        UniversityCreator universityCreator = new UniversityCreator();
+        University university = universityCreator.createUniversityRandomly();
+        universityJSONReaderWriter.writeToFile(university, "./src/main/resources/lab4.txt" );
+        University universityFromFile = universityJSONReaderWriter.readFromFile("./src/main/resources/lab4.txt");
+
+        assertEquals(universityFromFile,university);
+    }
+
+
+
+
+}
